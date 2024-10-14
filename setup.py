b4803c87f31296557ed0b9dcc0d80dbc5bdbb1e1
Ella Afonso <daniella.afonso@outlook.com>
2024-10-14 12:59:55 +0100

    Dataset Added For Visa Prediction

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Prediction_Of_US_Visa",
+    version="0.0.0",
+    author="Ella",
+    author_email="daniella.afonso@outlook.com",
+    packages=find_packages()
+
+
+
+)
---
+++ test.py
@@ -0,0 +1,3 @@
+from Prediction_Of_US_Visa.components.data_ingestion import main
+
+main()
